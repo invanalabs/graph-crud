70b3eb2c7b5b34b325c24a7786f99bf3ce28cbee
rrmerugu <rrmerugu@gmail.com>
2020-04-23 13:50:40 +0530

    tests for vertices added

--- graph_crud/operations/vertex.py
+++ graph_crud/operations/vertex.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 class Vertex(OperationsBase):
 
     def validate_data(self, query_data):
-        required_fields = ["type", "operation_type", "payload"]
+        required_fields = ["type", "operation_type", "data"]
         query_data_keys = query_data.keys()
         for required_field in required_fields:
             if required_field not in query_data_keys:
@@ -16,30 +16,30 @@ class Vertex(OperationsBase):
                     ",".join(required_fields)
                 ))
 
-        payload = query_data.get("payload")
-        if payload is None or type(payload) is not dict:
-            raise InvalidVertexException("Payload need to be dict, Refer documentation.")
+        data = query_data.get("data")
+        if data is None or type(data) is not dict:
+            raise InvalidVertexException("data need to be dict, Refer documentation.")
 
-        properties = query_data.get("payload", {}).get("properties")
+        properties = query_data.get("data", {}).get("properties")
         if properties is None or type(properties) is not dict:
             raise InvalidVertexException("properties need to be dict, Refer documentation.")
 
-        required_payload_fields = ["label", "properties"]
-        payload_keys = payload.keys()
-        for required_field in required_payload_fields:
-            if required_field not in payload_keys:
-                raise InvalidVertexException("In payload, these keys []; are required".format(
-                    ",".join(required_payload_fields)
+        required_data_fields = ["label", "properties"]
+        data_keys = data.keys()
+        for required_field in required_data_fields:
+            if required_field not in data_keys:
+                raise InvalidVertexException("In data, these keys []; are required".format(
+                    ",".join(required_data_fields)
                 ))
 
     def create(self, query_data):
         logger.debug("Creating vertex with query_data {query_data}".format(query_data=query_data))
-        payload = query_data.get("payload")
-        _ = self.g.addV(payload['label'])
-        for k, v in payload['properties'].items():
+        data = query_data.get("data")
+        _ = self.g.addV(data['label'])
+        for k, v in data['properties'].items():
             _.property(k, v)
-        _.next()
-        return _
+        _vtx = _.valueMap(True).next()
+        return self._serialize_vertex_data(_vtx)
 
     def update(self, query_data):
         logger.debug("Updating vertex with query_data {query_data}".format(query_data=query_data))
@@ -83,9 +83,12 @@ class Vertex(OperationsBase):
         logger.debug("Updating vertex with query_data {query_data}".format(query_data=query_data))
         query = query_data.get("query", {})
         filtered_data = self.filter(query)
-        _ = filtered_data.valueMap(True).toList()
-        if _.__len__() > 0:
-            return self._serialize_vertex_data(_[0])
+        try:
+            _ = filtered_data.valueMap(True).next()
+            if _:
+                return self._serialize_vertex_data(_)
+        except Exception as e:
+            pass
         return None
 
     def read_many(self, query_data):
--- tests/test_vertices.py
+++ tests/test_vertices.py
@@ -2,6 +2,56 @@ import pytest
 
 
 class TestVertex:
+    DATA_CREATE_QUERY = {
+        "type": "vertex",
+        "operation_type": "create",
+        "data": {
+            "label": "Plant",
+            "properties": {
+                "common_name": "chrysanths",
+                "scientific_name": "Chrysanthemum"
+            }
+        }
+    }
+
+    UPDATE_QUERY = {
+        "type": "vertex",
+        "operation_type": "update",
+        "data": {
+            "label": "Plant",
+            "properties": {
+                "common_name": "chrysanths",
+                "scientific_name": "Chrysanthemum"
+            }
+        },
+        "query": {
+            "scientific_name": "Chrysanthemum"
+        }
+    }
+
+    READ_ONE_QUERY = {
+        "type": "vertex",
+        "operation_type": "read_one",
+        "query": {
+            "label": "Plant",  # optional not needed
+            "scientific_name": "Chrysanthemum"
+        }
+    }
+    READ_MANY_QUERY = {
+        "type": "vertex",
+        "operation_type": "read_many",
+        "query": {
+            "label": "Plant",  # optional not needed
+            "scientific_name": "Chrysanthemum"
+        }
+    }
+    DELETE_QUERY = {
+        "type": "vertex",
+        "operation_type": "delete",
+        "query": {
+            "scientific_name": "Chrysanthemum"
+        }
+    }
 
     @pytest.fixture
     def graph_manager(self):
@@ -9,25 +59,21 @@ class TestVertex:
         return CrudManager("ws://127.0.0.1:8182/gremlin")
 
     def test_create_vertex(self, graph_manager):
-        msg = {
-            "type": "vertex",
-            "operation_type": "create",
-            "payload": {
-                "label": "Plant",
-                "properties": {
-                    "common_name": "chrysanths",
-                    "scientific_name": "Chrysanthemum"
-                }
-            }
-        }
+        msg = self.DATA_CREATE_QUERY
+        vtx = graph_manager.process(msg)
+
+    def test_updated_vertex(self, graph_manager):
+        msg = self.UPDATE_QUERY
+        vtx = graph_manager.process(msg)
+
+    def test_read_one_vertex(self, graph_manager):
+        msg = self.READ_ONE_QUERY
+        vtx = graph_manager.process(msg)
+
+    def test_read_vertex(self, graph_manager):
+        msg = self.READ_MANY_QUERY
         vtx = graph_manager.process(msg)
 
     def test_delete_vertex(self, graph_manager):
-        msg = {
-            "type": "vertex",
-            "operation_type": "delete",
-            "query": {
-                "id": 8248
-            }
-        }
+        msg = self.DELETE_QUERY
         graph_manager.process(msg)
